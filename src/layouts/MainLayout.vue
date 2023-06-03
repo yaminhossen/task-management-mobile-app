10abd2b1e5ba46248b0ec32d7175861c80cd2073
yaminhossen <streatdot616@gmail.com>
2023-06-03 21:42:28 +0600

    first comit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,150 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header >
+     
+        
+        <div class="navbar">
+          <div>
+            
+        <button class="menu"  @click="toggleLeftDrawer"><i class="fa-solid fa-bars"></i></button>
+        </div>
+       
+
+        <div class="logo">
+          <img src="expert-1.jpg" alt="">
+        </div>
+        </div>
+      
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+      <div>
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+<style scoped>
+.navbar{
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  padding-top: 10px;
+  /* border: 1px solid red; */
+  align-items: center;
+  
+  
+}
+.menu{
+  color: rgb(38, 35, 35);
+  margin-left: 20px;
+  background-color: white;
+  font-size: 18px;
+  margin-bottom: 5px;
+ border: 1px solid rgb(14, 65, 175);
+ border-radius: 5px;
+}
+.menu i{
+  /* font-size: 20px; */
+  /* background-color: white; */
+  /* padding: 2px; */
+  /* border: none; */
+  /* margin-bottom: 3px; */
+}
+.logo img{
+  width: 40px;
+  border-radius: 30%;
+  margin-right: 20px;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="banner">
+    <div class="btitle">
+      Manage your <br> time well
+    </div>
+    <div class="blogo">
+      <i class="fa-regular fa-file"></i>
+    </div>
+  </div>
+  <div class="categories">
+    <h5>Categories</h5>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+<style scoped>
+.banner{
+  background-image: linear-gradient(to right, #757dedb7, #4c4a4892);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 4px 35px;
+  margin: 20px 15px;
+  border-radius: 15px;
+}
+.btitle{
+  color: white;
+  font-size: 16px;
+}
+.blogo{
+  font-size: 54px;
+  color: rgb(197, 200, 243);
+}
+</style>
